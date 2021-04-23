8788eb8a13681fb7a52e2f9424762b7a8986400f
Emmanuel <rivera128@live.com>
2021-04-22 21:43:33 -0500

    commit tree 706a5579d207c2f6d2e281662ac4a5a5c2fc5b95

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,12 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def new
+  end
+
+  def create
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to students_path
+  end
 end
